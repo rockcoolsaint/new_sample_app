cc153617ab5c03947f1732c1c83657d56d63a171
“innocent <rockcoolsaint@gmail.com>
2016-07-11 15:57:08 +0100

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -11,7 +13,13 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'
 
-  post 'signup' => 'users#create'
+  #post 'signup' => 'users#create'
+
+  get '/login' => 'sessions#new'
+
+  post '/login' => 'sessions#create'
+
+  delete '/logout' => 'sessions#destroy'
 
   resources :users
 
