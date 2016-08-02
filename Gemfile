5fed3778f97b01b3f4e7ea2b35fa060e08ab8a6a
“innocent <rockcoolsaint@gmail.com>
2016-08-02 19:56:46 +0100

    gems on master branch modified

--- Gemfile
+++ Gemfile
@@ -7,6 +7,12 @@ gem 'rails', '4.2.2'
 gem 'bcrypt', '3.1.9'
 #gem 'bootstrap-sass', '3.2.0.0'
 
+gem 'faker',          '1.6.3'
+gem 'carrierwave',             '0.11.2'
+gem 'mini_magick',             '4.5.1'
+gem 'fog',                     '1.38.0'
+gem 'will_paginate',           '3.0.7'
+
 gem 'coffee-script-source', '1.8.0'
 # Use sqlite3 as the database for Active Record
 #gem 'sqlite3', group: [:development, :test]
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627135847) do
+ActiveRecord::Schema.define(version: 20160801212311) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +39,7 @@ ActiveRecord::Schema.define(version: 20160627135847) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
