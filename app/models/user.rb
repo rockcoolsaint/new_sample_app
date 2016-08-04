27b1539ccff5020e49f882d168d7340f9bc6f464
“innocent <rockcoolsaint@gmail.com>
2016-08-04 10:28:59 +0100

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	attr_accessor :remember_token
+
 	has_many :microposts, dependent: :destroy
 	has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
@@ -8,8 +10,6 @@ class User < ActiveRecord::Base
                                    dependent:   :destroy
     has_many :following, through: :active_relationships, source: :followed
 	has_many :followers, through: :passive_relationships, source: :follower
-
-	attr_accessor :remember_token
 
 	before_save { self.email = email.downcase }
 	validates :name, presence: true, length: {maximum: 50}
@@ -54,12 +54,25 @@ class User < ActiveRecord::Base
 	    BCrypt::Password.create(string, cost: cost)
 	end
 
+	# Returns a random token.
 	def User.new_token
 		SecureRandom.urlsafe_base64
 	end
 
+	# Remembers a user in the database for use in persistent sessions.
 	def remember
 		self.remember_token = User.new_token
 		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	# Returns true if the given token matches the digest.
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+	    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	# Forgets a user.
+	def forget
+	    update_attribute(:remember_digest, nil)
 	end
 end
