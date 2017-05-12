ae7767813ddab0ce90891fd36c52daa6eba046b7
“innocent <rockcoolsaint@gmail.com>
2017-05-12 02:42:22 +0100

    email server changed to mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,15 +64,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'https://morning-ocean-98914.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :address              => "smtp.mailgun.org",
+    :port                 => 587,
+    :user_name            => ENV['MAILGUN_SMTP_LOGIN'],
+    :password             => ENV['MAILGUN_SMTP_PASSWORD'],
+    :authentication       => "plain",
+    :domain         => ENV['MAILGUN_DOMAIN'],
     :enable_starttls_auto => true
   }
 
